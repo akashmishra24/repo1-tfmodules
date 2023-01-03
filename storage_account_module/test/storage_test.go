5b040c001c432e320585f880750967304d63863e
Abhishek Kumar <AKuma630@its.jnj.com>
2023-01-03 17:49:25 +0530

    updated sp

--- storage_account_module/test/storage_test.go
+++ storage_account_module/test/storage_test.go
@@ -2,43 +2,43 @@ package test
 
 import (
 	"testing"
-	"os"
+	//"os"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 )
 
-var (
-	globalEnvVars = make(map[string]string)
-)
+//var (
+//	globalEnvVars = make(map[string]string)
+//)
 
 
-func setTerraformVariables() (map[string]string, error) {
-
-	// Getting enVars from environment variables
-	ARM_CLIENT_ID := os.Getenv("AZURE_CLIENT_ID")
-	ARM_CLIENT_SECRET := os.Getenv("AZURE_CLIENT_SECRET")
-	ARM_TENANT_ID := os.Getenv("AZURE_TENANT_ID")
-	ARM_SUBSCRIPTION_ID := os.Getenv("AZURE_SUBSCRIPTION_ID")
-
-	// Creating globalEnVars for terraform call through Terratest
-	if ARM_CLIENT_ID != "" {
-		globalEnvVars["AZURE_CLIENT_ID"] = ARM_CLIENT_ID
-		globalEnvVars["AZURE_CLIENT_SECRET"] = ARM_CLIENT_SECRET
-		globalEnvVars["AZURE_SUBSCRIPTION_ID"] = ARM_SUBSCRIPTION_ID
-		globalEnvVars["AZURE_TENANT_ID"] = ARM_TENANT_ID
-	}
-
-	return globalEnvVars, nil
-}
+//func setTerraformVariables() (map[string]string, error) {
+//
+//	// Getting enVars from environment variables
+//	ARM_CLIENT_ID := os.Getenv("AZURE_CLIENT_ID")
+//	ARM_CLIENT_SECRET := os.Getenv("AZURE_CLIENT_SECRET")
+//	ARM_TENANT_ID := os.Getenv("AZURE_TENANT_ID")
+//	ARM_SUBSCRIPTION_ID := os.Getenv("AZURE_SUBSCRIPTION_ID")
+//
+//	// Creating globalEnVars for terraform call through Terratest
+//	if ARM_CLIENT_ID != "" {
+//		globalEnvVars["AZURE_CLIENT_ID"] = ARM_CLIENT_ID
+//		globalEnvVars["AZURE_CLIENT_SECRET"] = ARM_CLIENT_SECRET
+//		globalEnvVars["AZURE_SUBSCRIPTION_ID"] = ARM_SUBSCRIPTION_ID
+//		globalEnvVars["AZURE_TENANT_ID"] = ARM_TENANT_ID
+//	}
+//
+//	return globalEnvVars, nil
+//}
 
 func TestTerraformStorageAcct(t *testing.T) {
-	setTerraformVariables()
+	//setTerraformVariables()
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		// Set the path to the Terraform code that will be tested.
 		TerraformDir: "./",
 		VarFiles:     []string{"terratest_input.tfvars"},
 		// globalvariables for user account
-		EnvVars: globalEnvVars,
+		//EnvVars: globalEnvVars,
 	})
 	
 	// GenOutput generates the output string to be asserted against
