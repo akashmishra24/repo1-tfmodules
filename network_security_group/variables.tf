9f7c471877654b241a07977425a928cdbfbbd3f6
akashmishra24 <89220556+akashmishra24@users.noreply.github.com>
2023-03-01 15:39:15 +0530

    Create variables.tf

---
+++ network_security_group/variables.tf
@@ -0,0 +1,49 @@
+variable "tags" {
+  type        = map(any)
+  description = "The tag values for the deployment"
+}
+
+variable "nsg_prefix" {
+  type        = string
+  description = "Name of the Network Security Group"
+  default = "test"
+}
+
+variable "nsg_rules" {
+  #  type = list(object({
+  #    name                       = string
+  #    priority                   = number
+  #    direction                  = string
+  #    access                     = string
+  #    protocol                   = string
+  #    source_port_range          = string
+  #    destination_port_range     = string
+  #    source_address_prefix      = string
+  #    destination_address_prefix = string
+  #  }))
+  description = "The values for each NSG rule "
+  default     = {}
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "The name of resource group"
+}
+
+variable "subnet_id_nsg" {
+  description = "Subnet ID that will be associated with the Route Table"
+  default     = {}
+}
+
+variable "subnet_ids" {
+ 
+  default = {}
+}
+
+variable "workload" {
+  default = "webapp"
+}
+
+variable "environment" {
+  default = "non-prod"
+}
