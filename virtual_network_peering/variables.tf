53c8386f783cd3ed42358b49551a167a84d2abb7
akashmishra24 <89220556+akashmishra24@users.noreply.github.com>
2023-03-01 15:44:53 +0530

    Create variables.tf

---
+++ virtual_network_peering/variables.tf
@@ -0,0 +1,55 @@
+variable "src_vnet_name" {
+
+}
+
+variable "dest_vnet_name" {
+
+}
+
+variable "peering_src_name" {
+
+}
+
+variable "peering_dest_name" {
+
+}
+
+variable "src_vnet_rg" {
+
+}
+
+variable "dest_vnet_rg" {
+
+}
+
+variable "allow_virtual_src_network_access" {
+
+}
+
+variable "allow_forwarded_src_traffic" {
+
+}
+
+variable "allow_gateway_src_transit" {
+
+}
+
+variable "use_remote_src_gateway" {
+
+}
+
+variable "allow_virtual_dest_network_access" {
+
+}
+
+variable "allow_forwarded_dest_traffic" {
+
+}
+
+variable "allow_gateway_dest_transit" {
+
+}
+
+variable "use_remote_dest_gateway" {
+
+}
