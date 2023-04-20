20fc252e9a1475862a8ea330eb0ef5a740d66ad0
akashmishra24 <89220556+akashmishra24@users.noreply.github.com>
2023-04-20 14:27:43 +0530

    Update output.tf

--- key_vault/test/output.tf
+++ key_vault/test/output.tf
@@ -1,7 +1,19 @@
-output "resource_group_name" {
-  value = module.resource_group_test.name
+output "resource_group" {
+  value = module.key_vault_test.resource_group
 }
 
 output "key_vault_name" {
-  value = module.key_vault_test.name
+  value = module.key_vault_test.key_vault_name
+}
+  
+output "secret_name" {
+  value = module.key_vault_test.secret_name
+}
+
+output "key_name" {
+  value = module.key_vault_test.key_name
+}
+
+output "certificate_name" {
+  value = module.key_vault_test.certificate_name
 }
