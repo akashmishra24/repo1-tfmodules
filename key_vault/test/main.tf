5e3205d12107fba8c464d45193f1a400e4c70f29
akashmishra24 <89220556+akashmishra24@users.noreply.github.com>
2023-04-20 14:08:29 +0530

    Update main.tf

--- key_vault/test/main.tf
+++ key_vault/test/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "test" {
   location = local.location
 }
 
-module "Key_vault_test" {
+module "key_vault_test" {
   source              = "../"
   resource_group_name = azurerm_resource_group.test.name
   #storage_account_ids_map          = module.StorageAccount.sa_ids_map
