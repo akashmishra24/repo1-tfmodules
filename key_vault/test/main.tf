3d4ccaa1c0d6452b345793537ff45963fefd269e
akashmishra24 <89220556+akashmishra24@users.noreply.github.com>
2023-04-20 14:45:20 +0530

    Update main.tf

--- key_vault/test/main.tf
+++ key_vault/test/main.tf
@@ -29,56 +29,7 @@ module "key_vault_test" {
   ]
 }
   
-  
-  resource "azurerm_key_vault" "key_vault" {
-  name                        = "keyvault-${var.postfix}"
-  location                    = azurerm_resource_group.resource_group.location
-  resource_group_name         = azurerm_resource_group.resource_group.name
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-
-  soft_delete_retention_days = 7
-  purge_protection_enabled   = false
-
-  sku_name = "standard"
-
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
-    key_permissions = [
-      "create",
-      "get",
-      "list",
-      "delete",
-      "purge",
-    ]
-
-    secret_permissions = [
-      "set",
-      "get",
-      "list",
-      "delete",
-      "purge",
-    ]
-
-    certificate_permissions = [
-      "create",
-      "delete",
-      "deleteissuers",
-      "get",
-      "getissuers",
-      "import",
-      "list",
-      "listissuers",
-      "managecontacts",
-      "manageissuers",
-      "setissuers",
-      "update",
-      "purge",
-    ]
-  }
-}
+ 
 
 # ---------------------------------------------------------------------------------------------------------------------
 # DEPLOY A SECRET TO THE KEY VAULT
