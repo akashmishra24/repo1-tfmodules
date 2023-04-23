b8c0af62e80c5e4bab8297dc0fd727f607e88c4b
akashmishra24 <89220556+akashmishra24@users.noreply.github.com>
2023-04-23 13:56:10 +0530

    Update output.tf

--- key_vault/output.tf
+++ key_vault/output.tf
@@ -1,21 +1,21 @@
 
 
 output "resource_group_name" {
-  value = module.key_vault_test.resource_group_name
+   value = [for x in azurerm_key_vault.key-vault : x.resource_group_name]
 }
 
 output "key_vault_name" {
-  value = module.key_vault_test.name
+  value = [for x in azurerm_key_vault.key-vault : x.name]
 }
   
 output "secret_name" {
-  value = module.key_vault_test.secret_name
+  value = [for x in azurerm_key_vault.key-vault : x.secret_name]
 }
 
 output "key_name" {
-  value = module.key_vault_test.key_name
+  value = [for x in azurerm_key_vault.key-vault : x.key_name]
 }
 
 output "certificate_name" {
-  value = module.key_vault_test.certificate_name
+  value = [for x in azurerm_key_vault.key-vault : x.certificate_name]
 }
