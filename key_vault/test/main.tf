e616d2a80f43761846068274a489ea22ab62c519
akashmishra24 <89220556+akashmishra24@users.noreply.github.com>
2023-04-24 17:08:47 +0530

    Update main.tf

--- key_vault/test/main.tf
+++ key_vault/test/main.tf
@@ -8,16 +8,21 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_key_vault_certificate" "certificate_name" {
-  name = var.certificate_name
+  name         = var.certificate_name
+  key_vault_id = azurerm_key_vault.key-vault.id
   
 }
 
 resource "azurerm_key_vault_secret" "this" {
-  name = var.secret_name
+  name         = var.secret_name
+  key_vault_id = azurerm_key_vault.key-vault.id
 }
 
 resource "azurerm_key_vault_key" "key_name" {
-  name = var.key_name
+  name         = var.key_name
+  key_vault_id = azurerm_key_vault.key-vault.id
+  key_type     = "RSA"
+  key_size     = 2048
 }
 
 module "key_vault_test" {
