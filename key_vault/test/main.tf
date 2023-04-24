f5fe7d3b433901b6d68032a614f1bc5988453dfe
akashmishra24 <89220556+akashmishra24@users.noreply.github.com>
2023-04-24 16:16:49 +0530

    Update main.tf

--- key_vault/test/main.tf
+++ key_vault/test/main.tf
@@ -21,6 +21,9 @@ module "key_vault_test" {
   tenant_id                       = var.tenant_id
   #access_policies                 = var.access_policies
   network_acls                    = var.network_acls
+  secret_name                     = var.secret_name
+  certificate_name                = var.certificate_name
+  key_name                        = var.key_name
   #log_analytics_workspace_id       = var.log_analytics_workspace_id
   #diagnostics_storage_account_name = var.diagnostics_storage_account_name
   #kv_additional_tags               = var.additional_tags
