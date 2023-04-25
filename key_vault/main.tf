1bf5287cefe40cfd01f11007840dd4cf3aaca76b
akashmishra24 <89220556+akashmishra24@users.noreply.github.com>
2023-04-25 15:32:09 +0530

    Update main.tf

--- key_vault/main.tf
+++ key_vault/main.tf
@@ -67,7 +67,7 @@ resource "azurerm_key_vault" "key-vault" {
 }
 
 resource "azurerm_key_vault_access_policy" "key_vault_access_policy" {
-  for_each                = var.enable_rbac_authorization ? {} : var.kv_access_policy
+  for_each                = var.kv_access_policy
   key_vault_id            = azurerm_key_vault.key-vault.id
   tenant_id               = data.azurerm_client_config.current.tenant_id
   object_id               = data.azurerm_client_config.current.object_id
