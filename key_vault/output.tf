0622e45f9dd7655be513928835109fdcc538509e
akashmishra24 <89220556+akashmishra24@users.noreply.github.com>
2023-04-26 14:50:14 +0530

    Update output.tf

--- key_vault/output.tf
+++ key_vault/output.tf
@@ -1,14 +1,7 @@
-
-
-output "resource_group_name" {
-   value = data.azurerm_resource_group.rg.name
-}
-
-output "key_vault_name" {
-  description = "The Name of the Key Vault"
-  value       = azurerm_key_vault.key-vault.name
-}
-   
- output "key_vault_id" {
+output "kv_id" {
   value = azurerm_key_vault.key-vault.id
+}
+
+output "key_vault_url" {
+  value = azurerm_key_vault.key-vault.vault_uri
 }
