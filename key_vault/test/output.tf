417b62bfd5420d1d6488568f1d59cae5f8402787
akashmishra24 <89220556+akashmishra24@users.noreply.github.com>
2023-04-26 16:32:16 +0530

    Update output.tf

--- key_vault/test/output.tf
+++ key_vault/test/output.tf
@@ -10,7 +10,7 @@ output "key_vault_name" {
   value = azurerm_key_vault.key-vault.name
 }
 
-Output "key_vault_id" {
+output "key_vault_id" {
   description = "Key Vault ID"
   value       = module.key_vault_test.key_vault_id
 }
