43f8d818f5630c8b1122f6dc5e3fee3e1240650e
akashmishra24 <89220556+akashmishra24@users.noreply.github.com>
2023-04-26 14:37:23 +0530

    Update main.tf

--- key_vault/test/main.tf
+++ key_vault/test/main.tf
@@ -73,6 +73,7 @@ module "key_vault_test" {
   virtual_network_name            = var.virtual_network_name
   tenant_id                       = var.tenant_id
   #access_policies                 = var.access_policies
+  public_network_access_enabled   = var.public_network_access_enabled
   network_acls                    = var.network_acls
   #secret_name                     = var.secret_name
   #certificate_name                = var.certificate_name
