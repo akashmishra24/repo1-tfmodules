2545d54e743d28dfc5aac4601a5a75f8bb35acf9
akashmishra24 <89220556+akashmishra24@users.noreply.github.com>
2023-04-27 11:57:04 +0530

    Update output.tf

--- key_vault/test/output.tf
+++ key_vault/test/output.tf
@@ -1,13 +1,16 @@
 output "kv_id" {
-  value = azurerm_key_vault.key-vault.id
+  #value = azurerm_key_vault.key-vault.id
+  value = module.key_vault_test.key_vault_id
 }
 
 output "key_vault_url" {
-  value = azurerm_key_vault.key-vault.vault_uri
+  #value = azurerm_key_vault.key-vault.vault_uri
+  value = module.key_vault_test.vault_uri
 }
 
 output "key_vault_name" {
-  value = azurerm_key_vault.key-vault.name
+  #value = azurerm_key_vault.key-vault.name
+  value = module.key_vault_test.name
 }
 
 
