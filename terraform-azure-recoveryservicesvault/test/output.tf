3088e87ee04b09b303ab870e831f74aa03cbaed1
bindu <bindu.maheswari5@gmail.com>
2023-05-04 21:09:49 +0530

    output

--- terraform-azure-recoveryservicesvault/test/output.tf
+++ terraform-azure-recoveryservicesvault/test/output.tf
@@ -3,9 +3,9 @@ output "resource_group_name" {
 }
 
 output "recovery_service_vault_name" {
-  value = azurerm_recovery_services_vault.this.name
+  value = azurerm_recovery_services_vault.this[each.key].name
 }
 
 output "backup_policy_vm_name" {
-  value = azurerm_backup_policy_vm.this.name
+  value = azurerm_backup_policy_vm.this[each.key].name
 }
