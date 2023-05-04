537963bfb2cd964c108d1c5ab6a7da0a1bd5a321
bindu <bindu.maheswari5@gmail.com>
2023-05-04 16:08:16 +0530

    output

--- terraform-azure-load-balancer/test/output.tf
+++ terraform-azure-load-balancer/test/output.tf
@@ -2,32 +2,12 @@ output "lb_private_name" {
   value = azurerm_lb.azlb.name
 }
 
-output "lb_private_fe_config_static_name" {
-  value = azurerm_lb.azlb.frontend_ip_configuration[0].name
-}
-
-output "lb_private_fe_config_dynamic_name" {
-  value = azurerm_lb.azlb.frontend_ip_configuration[1].name
-}
-
-output "lb_private_ip_static" {
-  value = azurerm_lb.azlb.frontend_ip_configuration[0].private_ip_address
-}
-
-output "lb_private_ip_dynamic" {
-  value = azurerm_lb.azlb.frontend_ip_configuration[1].private_ip_address
-}
-
 output "lb_public_name" {
-  value = azurerm_public_ip.azlb[0].name
-}
-
-output "lb_public_fe_config_name" {
-  value = azurerm_lb.azlb.frontend_ip_configuration[0].name
+  value = azurerm_public_ip.azlb.name
 }
 
 output "public_address_name" {
-  value = azurerm_public_ip.azlb[0].name
+  value = azurerm_public_ip.azlb.name
 }
 
 output "resource_group_name" {
