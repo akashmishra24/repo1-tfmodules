7250a3684be291b7a7d8a8033e0dedbf6a5b4b89
bindu <bindu.maheswari5@gmail.com>
2023-05-10 15:29:05 +0530

    nsg

--- terraform-azure-network-security-group/test/main.tf
+++ terraform-azure-network-security-group/test/main.tf
@@ -41,7 +41,8 @@ resource "azurerm_network_security_group" "nsg" {
 resource "azurerm_subnet_network_security_group_association" "subnet_nsg_association" {
   for_each                  = var.subnet_ids
   subnet_id                 = each.value["subnet_id"]
-  network_security_group_id = azurerm_network_security_group.nsg.id
+  network_security_group_id = lookup(azurerm_network_security_group.this, each.value["nsg_key"], null)["id"]
+
   depends_on = [
     azurerm_network_security_group.nsg
   ]
