665ef3f976c03a3c0adaaf1299ada23d2d539feb
bindu <bindu.maheswari5@gmail.com>
2023-05-10 15:01:16 +0530

    nsg

--- terraform-azure-network-security-group/test/main.tf
+++ terraform-azure-network-security-group/test/main.tf
@@ -13,7 +13,7 @@ resource "azurerm_network_security_group" "nsg" {
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
   dynamic "security_rule" {
-    for_each = var.nsg_rules
+    for_each = lookup(each.value, "security_rules", [])
     content {
       name                         = security_rule.value["name"]
       priority                     = security_rule.value["priority"]
